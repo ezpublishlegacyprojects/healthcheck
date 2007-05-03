4e21c30e5aef0a7861904cc678e449112689ce7e
mgdm <info@example.com>
2007-05-03 16:42:34 +0000

    Remove extraneous INI reading code
    git-svn-id: http://svn.projects.ez.no/healthcheck@4 b690ac95-d32e-4001-a601-1e0304cb7d17

--- trunk/healthcheck/modules/healthcheck/overview.php
+++ trunk/healthcheck/modules/healthcheck/overview.php
@@ -5,18 +5,7 @@ include_once( 'lib/ezutils/classes/ezini.php' );
 
 $module = $Params['Module'];
 
-$myIni =& eZINI::instance( 'healthcheck.ini' );
-$tests = $myIni->variableArray( "AvailableTests", "Tests" );
-
-$testArray = array();
-foreach( $tests as $test ) {
-    $testArray[] = array( "view_name" => $test[0],
-                          "friendly_name" => $myIni->variable( $test[0], "Name" ),
-    );
-}
-
 $tpl =& templateInit();
-$tpl->setVariable( 'tests', $testArray );
 $tpl->setVariable( 'module_name', 'healthcheck' );
 
 $Result['path'] = array( array( 'url' => '/healthcheck/overview', 'text' => "System Health Check" ),
